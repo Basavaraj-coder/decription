5c2f9fbc59346f9b88c6d271ec98f6d19de59cb8
Basavaraj Hiremath <78433317+Basavaraj-coder@users.noreply.github.com>
2021-08-14 14:30:42 +0530

    First commit

---
+++ demo1.py
@@ -0,0 +1,15 @@
+print("hii this is first program for my frnds")
+a=None
+type(a)
+b=988899870058694
+print(type(b))
+c=10.10
+print(type(c)) #type is for check data type
+d=False  # u may give true or false value 
+print(type(d))
+e="Hello "
+print(type(e))
+d=2
+print(type(d))
+d1=bytes(d)
+print(type(d1))
---
+++ detectSpaces.py
@@ -0,0 +1,3 @@
+str=input("Enter a string \n ")
+sp=str.find(" ")
+print("space is detected")
